58b3208fd46319270460121e640ce9fd595412bf
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-29 23:33:56 +0800

    Update Class14.java

--- Class14.java
+++ Class14.java
@@ -3,8 +3,11 @@ public class Class14 {
     public static void main(String[] args) {
         try{
             Scanner sc=new Scanner(System.in); 
+            
             String a=sc.nextLine();
+            sc.close();
             int b=Integer.valueOf(a);
+          
             if(b>=10 & b<=70){
                 System.out.println(b);
             }
@@ -24,5 +27,6 @@ public class Class14 {
         catch(NumberFormatException ex){
             System.out.println("您輸入的不是整數");
         }
+        
     }
 }

ef61a6ff97a75315ee06390266651c34b6d3a988
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-29 23:27:02 +0800

    Create Class14.java

---
+++ Class14.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Class14 {
+    public static void main(String[] args) {
+        try{
+            Scanner sc=new Scanner(System.in); 
+            String a=sc.nextLine();
+            int b=Integer.valueOf(a);
+            if(b>=10 & b<=70){
+                System.out.println(b);
+            }
+            else if(b>70){
+                throw new ArithmeticException();
+            }
+            else if(b<10){
+                throw new ArrayStoreException();
+            }
+        }
+        catch(ArithmeticException e){
+            System.out.println("您輸入的整數太大");
+        }
+        catch(ArrayStoreException ex){
+            System.out.println("您輸入的整數太小");
+        }
+        catch(NumberFormatException ex){
+            System.out.println("您輸入的不是整數");
+        }
+    }
+}
